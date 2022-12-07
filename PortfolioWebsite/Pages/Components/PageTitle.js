4067668208a4e763c4003776492789cac970de84
Kenneth Munk <da.munkk@gmail.com>
2022-12-07 09:39:12 -0800

    page title font scalable

--- PortfolioWebsite/Pages/Components/PageTitle.js
+++ PortfolioWebsite/Pages/Components/PageTitle.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
-import { Text, View} from 'react-native';
+import { View, useWindowDimensions, Text, Image } from 'react-native';
 import PortfolioStyle from './PortfolioStyle';
 
 export default function PageTitle(props) {
+	const {height, width} = useWindowDimensions();
+	const paperRatio = 1.294;
+	const buffer = (70 * (width/700)) + 250;
+	const referencePage = 1440;
+	const scaleValue = (width/referencePage)<1 ? (width/referencePage) : 1;
 	
 	return(
 		
-		<Text style={[PortfolioStyle.pageTitleBox,]}>
+		<Text style={[PortfolioStyle.pageTitleBox,{fontSize: 60*scaleValue,}]}>
 			{props.children}
 		</Text>
 	);
